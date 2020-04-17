13f944ac5280ee21f04b34ad11ca7f2fb052d344
Gene Merewether <gmerewether@bostondynamics.com>
2020-04-17 13:22:37 -0400

    [DrSpot] Add uvc video ko options and update to default optris thermal IR

--- vitals_collection/drspot/scripts/config/etc/ros/env.sh
+++ vitals_collection/drspot/scripts/config/etc/ros/env.sh
@@ -3,4 +3,4 @@ export PUREGEV_ROOT=/opt/pleora/ebus_sdk/Ubuntu-x86_64
 export GENICAM_ROOT=$PUREGEV_ROOT/lib/genicam
 export GENICAM_ROOT_V3_0=$GENICAM_ROOT
 
-export DRSPOT_THERMAL_NS="flir_camera"
+export DRSPOT_THERMAL_NS="optris"
