2bc4e2cc2c8ce597574adfb440aa1e900324097c
Gene Merewether <gmerewether@bostondynamics.com>
2020-04-30 21:32:55 -0400

    [DrSpot] Add more troubleshooting to script

--- vitals_collection/drspot/scripts/config/home/spot/.local/bin/troubleshoot_drspot.bash
+++ vitals_collection/drspot/scripts/config/home/spot/.local/bin/troubleshoot_drspot.bash
@@ -26,18 +26,21 @@ done
 
 printf 'Please save "%s" and "%s" for troubleshooting.\n' "$outfile" "$errfile"
 
+DISPLAY=:16001
+
 function exec_and_log() {
-    set -x
-    echo "================================================================================" >&3
-    echo "================================================================================" >&4
+    echo "********************************************************************************" >&3
+    echo "********************************************************************************" >&4
     echo "executing $@" >&3
     echo "executing $@" >&4
     echo "--------------------------------------------------------------------------------" >&3
     echo "--------------------------------------------------------------------------------" >&4
-    timeout 10 $@ >&3 2>&4
-    echo "================================================================================" >&3
-    echo "================================================================================" >&4
+    TIMEOUT=${TIMEOUT:-20}
+    set -x
+    timeout $TIMEOUT $@ >&3 2>&4
     set +x
+    echo "********************************************************************************" >&3
+    echo "********************************************************************************" >&4
 }
 
 # Environment variables
@@ -50,9 +53,11 @@ exec_and_log "systemctl status roslaunch.service"
 exec_and_log "roswtf"
 
 # ROS nodes
+TIMEOUT=1
 exec_and_log "rostopic hz /${DRSPOT_THERMAL_NS}/thermal_image_raw"
 exec_and_log "rostopic hz /${DRSPOT_THERMAL_NS}/temperature_image"
 exec_and_log "rostopic hz /${DRSPOT_THERMAL_NS}/thermal_palette"
+unset TIMEOUT
 
 # Devices
 exec_and_log "df -h"
@@ -61,10 +66,10 @@ exec_and_log "ifconfig"
 exec_and_log "ip route"
 exec_and_log "lsusb"
 exec_and_log "/usr/bin/ir_find_serial"
+exec_and_log "cat /sys/class/thermal/thermal_zone*/temp"
+exec_and_log "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq"
+exec_and_log "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq"
 
 # VNC
 exec_and_log "systemctl status vncserver@16001.service"
-exec_and_log "DISPLAY=:16001 /home/spot/.local/bin/run_drspot_gui.bash"
-
-# Misc / System
-exec_and_log "dmesg"
+exec_and_log "/home/spot/.local/bin/run_drspot_gui.bash"
