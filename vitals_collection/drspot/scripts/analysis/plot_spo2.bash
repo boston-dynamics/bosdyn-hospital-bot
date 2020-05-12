f4cd7b191a31661d415108c129326d918e3c0645
Gene Merewether <gmerewether@bostondynamics.com>
2020-05-12 13:26:37 -0400

    [DrSpot] Add plot script for spo2 output comparison

---
+++ vitals_collection/drspot/scripts/analysis/plot_spo2.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+gnuplot -p -e "pulse='$1/avg_pulse.csv'; \
+              freq='$1/avg_pulse_fft.csv'" \
+        ${DIR}/spo2.gnuplot
